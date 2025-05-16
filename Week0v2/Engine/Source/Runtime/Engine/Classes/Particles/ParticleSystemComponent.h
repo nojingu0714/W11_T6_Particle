ce61e09976f6e6567cd3188a7b63e9dd3f36a43c
Hellowhales <hellowhales7@gmail.com>
2025-05-16 22:34:24 +0900

    feat : Particle System Component Values

--- Week0v2/Engine/Source/Runtime/Engine/Classes/Particles/ParticleSystemComponent.h
+++ Week0v2/Engine/Source/Runtime/Engine/Classes/Particles/ParticleSystemComponent.h
@@ -1,10 +1,18 @@
 ﻿#pragma once
 #include "FXSystemComponent.h"
 
+struct FDynamicEmitterDataBase;
+class UParticleSystem;
+
 class UParticleSystemComponent : public UFXSystemComponent
 {
     DECLARE_CLASS(UParticleSystemComponent, UFXSystemComponent)
 public:
     UParticleSystemComponent() = default;
     ~UParticleSystemComponent() = default;
+
+    TArray<struct FParticleEmitterInstance*> EmitterInstances;
+    UParticleSystem* Template;
+
+    TArray<FDynamicEmitterDataBase*> EmitterRenderData;
 };
